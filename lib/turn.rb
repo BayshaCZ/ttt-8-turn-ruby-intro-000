529e67056c86e0fd2e74d4a82f3af3af0b6724f4
Eva Podbrdská <podbrdska@gmail.com>
2020-08-13 05:01:50 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,16 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if (valid_move?(board, index) == true)
+    move (board, index, token = "X")
+  else
+    turn(board)
+  end
+end
 
 def input_to_index(input)
   input.to_i - 1
