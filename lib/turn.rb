a3860029b92219ff371a6c7595b9127ad364db80
Eva Podbrdská <podbrdska@gmail.com>
2020-08-13 05:12:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index) == true
-    move (board, index, token = "X")
+    move(board, index, token = "X")
   else
     turn(board)
   end
